c7a63a52b016283f77acb276cf73ea5385a693c5
David Chaves <dachafra@gmail.com>
2021-10-21 18:41:41 +0200

    adding support for all prefixes based on the onto name

--- translate.py
+++ translate.py
@@ -21,8 +21,8 @@ def translate():
         print("No input the correct arguments, run pip3 translate.py -h to see the help)")
         sys.exit()
 
-    #onto_path.append(os.path.abspath(ontology).replace(ontology, ""))
-    onto = get_ontology("file://"+os.path.abspath(ontology)).load()
+    # onto_path.append(os.path.abspath(ontology).replace(ontology, ""))
+    onto = get_ontology("file://" + os.path.abspath(ontology)).load()
 
     template = init_yarrrml()
 
@@ -38,15 +38,16 @@ def init_yarrrml():
 
 def construct_mapping(template, onto):
     base_iri = onto.base_iri
+    prefixes = generate_prefixes(template,onto)
     for c in list(onto.classes()):
-        template['prefixes']['ns'] = base_iri
+        template['prefixes'][onto.name] = base_iri
         triplesmapTemplate = copy.deepcopy(template['mappings']['triplesmap0'])
-        triplesmapTemplate['po'][0][1] = c.iri.replace(base_iri, 'ns:')
+        triplesmapTemplate['po'][0][1] = c.iri.replace(c.namespace.base_iri, prefixes[c.namespace.base_iri] + ":")
         del triplesmapTemplate['po'][1]
         class_name = c.iri.replace(c.namespace.base_iri, '')
         pos = 1
         for d in list(onto.data_properties()):
-            property_name = d.iri.replace('\'', '').replace(base_iri, 'ns:')
+            property_name = d.iri.replace('\'', '').replace(d.namespace.base_iri, prefixes[d.namespace.base_iri] + ":")
             for domains in d.domain:
                 if domains == c or domains in c.is_a:
                     for ranges in d.range:
@@ -60,22 +61,36 @@ def construct_mapping(template, onto):
         template['mappings']['triplesMap' + class_name.capitalize()] = triplesmapTemplate
     for c in list(onto.classes()):
         for triplesmap in dict(template['mappings']):
-            if template['mappings'][triplesmap]['po'][0][1] == (c.iri.replace(base_iri, 'ns:')):
+            if template['mappings'][triplesmap]['po'][0][1] == (c.iri.replace(c.namespace.base_iri, prefixes[c.namespace.base_iri] + ":")):
                 join_template = template['mappings']['triplesmap0']['po'][1]
-                generate_ref_object_maps(triplesmap, join_template, template, c, onto)
+                generate_ref_object_maps(triplesmap, join_template, template, c, onto,prefixes)
 
     del template['mappings']['triplesmap0']
 
 
-def find_triplesmap(mapping, base_iri, range):
+def generate_prefixes(template, onto):
+    prefixes = {}
+    for c in list(onto.classes()):
+        template['prefixes'][c.namespace.name] = c.namespace.base_iri
+        prefixes[c.namespace.base_iri] = c.namespace.name
+    for dp in list(onto.data_properties()):
+        template['prefixes'][dp.namespace.name] = dp.namespace.base_iri
+        prefixes[dp.namespace.base_iri] = dp.namespace.name
+    for do in list(onto.object_properties()):
+        template['prefixes'][do.namespace.name] = do.namespace.base_iri
+        prefixes[do.namespace.base_iri] = do.namespace.name
+    return prefixes
+
+
+def find_triplesmap(mapping, base_iri, range, onto):
     ref_triples_map = ""
     for triplesMap in dict.keys(mapping['mappings']):
-        if mapping['mappings'][triplesMap]['po'][0][1] == (range.iri.replace(base_iri, 'ns:')):
+        if mapping['mappings'][triplesMap]['po'][0][1] == (range.iri.replace(base_iri, onto.name)):
             ref_triples_map = triplesMap
     return ref_triples_map
 
 
-def generate_ref_object_maps(triplesmap, join_template, template, c, onto):
+def generate_ref_object_maps(triplesmap, join_template, template, c, onto,prefixes):
     for o in list(onto.object_properties()):
         for domain in o.domain:
             if domain == c:
@@ -83,22 +98,24 @@ def generate_ref_object_maps(triplesmap, join_template, template, c, onto):
                     if type(range) is not owlready2.entity.ThingClass:
                         for r in range.Classes:
                             if r is owlready2.entity.ThingClass:
-                                create_join_condition(template, onto, join_template, o, triplesmap, r)
+                                create_join_condition(template, onto, join_template, o, triplesmap, r,prefixes)
                     else:
                         if range.iri != "http://www.w3.org/2002/07/owl#Thing":
-                            create_join_condition(template, onto, join_template, o, triplesmap, range)
+                            create_join_condition(template, onto, join_template, o, triplesmap, range,prefixes)
 
 
-def create_join_condition(template, onto, join_template, o, triplesmap, range):
-    triples_map_parent = find_triplesmap(template, onto.base_iri, range)
+def create_join_condition(template, onto, join_template, o, triplesmap, range,prefixes):
+    triples_map_parent = find_triplesmap(template, onto.base_iri, range, onto)
     join = copy.deepcopy(join_template)
-    join['p'] = o.iri.replace(onto.base_iri, 'ns:')
+    join['p'] = o.iri.replace(o.namespace.base_iri, prefixes[o.namespace.base_iri] + ":")
     join['o'][0]['mapping'] = triples_map_parent
     template['mappings'][triplesmap]['po'].append(join)
 
 
 def write_output(mapping, output):
-    dumped_yaml = str(yaml.dump(mapping, default_flow_style=None, sort_keys=False)).replace("'\"", '"').replace("\"'",' " ').replace('\'', '')
+    dumped_yaml = str(yaml.dump(mapping, default_flow_style=None, sort_keys=False)).replace("'\"", '"').replace("\"'",
+                                                                                                                ' " ').replace(
+        '\'', '')
     with open(output, "w") as output_stream:
         output_stream.write(dumped_yaml)
 
